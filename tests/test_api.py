d641e47c8fd6fe19b1c6d7024d068c090903135f
SrilekhaRchandran <srilekharchandran@gmail.com>
2025-02-07 13:57:29 +0700

    test commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,9 @@ def test_api_status_code():
     assert response.status_code == 200, "API did not return a 200 status code"
 
 
+# test
 def test_api_response_content():
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
     data = response.json()
-    assert data["id"] == 1, "API response content is incorrect"
+    assert data["id"] == 1, "API response content is incorrect. Try Again"
+
